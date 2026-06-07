d75b9155cf6b5ff92d08f432dd127b025882cb99
Sahannny <sahan.jayatissa@auckland.ac.nz>
2026-06-08 10:39:53 +1200

    Slight changes to pressures and cleaned up functions

--- Generate_flow_data.py
+++ Generate_flow_data.py
@@ -20,8 +20,8 @@ from reprosim.pressure_resistance_flow import evaluate_prq, calculate_stats
 import csv
 import os
 
-sample_number = 'PE153'
-img_input_dir = '/Image_input/' +sample_number + '/' #'/media/share/derivative/2023-sex-specific/chorionic-segmentations/' +sample_number +'/'
+sample_number = 'JT23070'
+img_input_dir = '/media/share/derivative/2023-sex-specific/chorionic-segmentations/' +sample_number +'/'
 output_tree_dir = 'outputs_grow_tree/' + sample_number + '/'
 output_flow_dir = 'outputs_flow_tree/' + sample_number + '/'
 output_table_dir = 'outputs_branch_stats/' + sample_number + '/'
