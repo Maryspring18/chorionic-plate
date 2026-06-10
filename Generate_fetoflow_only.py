9cd1bca547c27b31392236675575ea3f83fc8954
Sahannny <sahan.jayatissa@auckland.ac.nz>
2026-06-11 03:56:33 +1200

    Bug fix of export as csv

---
+++ Generate_fetoflow_only.py
@@ -0,0 +1,39 @@
+from fetoflow import *
+
+sample_number = 'PN783'
+img_input_dir = 'Image_input/'
+output_tree_dir = 'outputs_grow_tree/' + sample_number + '/'
+output_flow_dir = 'outputs_flow_tree/' + sample_number + '/'
+output_table_dir = 'outputs_branch_stats/' + sample_number + '/'
+
+nodes = read_nodes(output_tree_dir + 'full_tree_' + sample_number + '.ipnode')
+elems = read_elements(output_tree_dir + 'full_tree_' + sample_number + '.ipelem')
+radii = define_fields_from_files({'radius':output_tree_dir+'tree_radii_'+sample_number+'.ipfiel'})
+
+
+outlet_pressure, inlet_flow, inlet_pressure = 2660, 4166.7, 6650
+bcs = generate_boundary_conditions(outlet_pressure=outlet_pressure, inlet_flow=inlet_flow)
+# define other required geometric features (radii and decay factors)
+umbilical_artery_radius, decay_factor = 1.8, 1.38  # 1.51795
+umbilical_vein_radius, decay_factor_vein = 4.0, 1.46
+arteries_only = False  # this should rarely be true
+viscosity_type = 'constant'  # can also be 'pries_network' or 'pries_vessel' if wanting to incorporate radius-dependence
+
+# Generate the di-graph & calculate the resistances based on the viscosity
+print("Creating Geometry")
+G = create_geometry(nodes, elems, umbilical_artery_radius, decay_factor, umbilical_vein_radius, decay_factor_vein,arteries_only=arteries_only, fields=radii, single_umbilical_vein=False)
+
+print("Calculating Resistance")
+G = calculate_resistance(G, viscosity_model=viscosity_type)
+print("Calculating Matrices")
+A, b, bc_export = create_small_matrices(G, bcs, branching_angles=False)
+print("Solving for Pressures and Flows")
+p, q = solve_small_system(A, b, G, bc_export)
+G = update_geometry_with_pressures_and_flows(G, p, q)
+inlet_measure, outlet_measure = get_tree_properties(G)
+export_region_as_csv(G, 'chorion',output_flow_dir+sample_number+'_ROI.csv', chorion_elems = None, order_interest= None)
+print(f"Total vessel volume is {calc_vessel_volume(G,'all')}, arterial vessel volume is {calc_vessel_volume(G, 'artery')}")
+#export_all(G, 'placenta', output_flow_dir + 'FF_' + sample_number, 'all')
+#export_field(G, 'placenta', 'strahler', output_flow_dir + 'FF_' + sample_number, 'all')
+visualise_tree(G, True, 'arteries')
+print('End of Code')
